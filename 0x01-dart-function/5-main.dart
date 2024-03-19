547bfcec31dbd5d13e1fde2074a2485858363f85
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-18 21:01:51 -0400

    Try new edge cases

--- 0x01-dart-function/5-main.dart
+++ 0x01-dart-function/5-main.dart
@@ -4,5 +4,7 @@ main() {
     print(calculateArea(20, 7));
     print(calculateArea(10, 7));
     print(calculateArea(8.5, 3.7));
+    print(calculateArea(-3.5, 79));
+    print(calculateArea(800, -3));
     print(calculateArea(-3.5, -0.6));
 }
