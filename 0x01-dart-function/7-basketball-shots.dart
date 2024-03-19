0265447eb32e2a6f623b8a7e8511620eb11e720f
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-19 15:29:47 -0400

    Document the function for Dart functions exercise 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,3 +1,5 @@
+/// Expects the maps to have 'Free throws', '2 pointers' and '3 pointers' as keys, with positive ints as values.
+/// If they're not present, or the value is null, the function defaults it to 0.
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
     int teamAScore = (teamA['Free throws'] ?? 0) + (teamA['2 pointers'] ?? 0) * 2 + (teamA['3 pointers'] ?? 0) * 3;
     int teamBScore = (teamB['Free throws'] ?? 0) + (teamB['2 pointers'] ?? 0) * 2 + (teamB['3 pointers'] ?? 0) * 3;
