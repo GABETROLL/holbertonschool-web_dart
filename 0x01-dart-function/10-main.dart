584b37a2b1beb2165571eca620854c770a272955
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-20 14:22:01 -0400

    Add Dart functions exercise 10

---
+++ 0x01-dart-function/10-main.dart
@@ -0,0 +1,14 @@
+import '10-longest-palindrome.dart';
+
+main() {
+    print(longestPalindrome("abcde"));
+    print(longestPalindrome("azeghjhg"));
+    print(longestPalindrome("aaabvf"));
+    print(longestPalindrome("hbhegfs;"));
+    print(longestPalindrome("awhfjdsh88h"));
+    print(longestPalindrome("0123456789876543210"));
+    print(longestPalindrome("012345678987654321"));
+    print(longestPalindrome("12345678987654321"));
+    print(longestPalindrome("1234567887654321"));
+    print(longestPalindrome("ababcbaaaaabcdedcba098978790"));
+}
