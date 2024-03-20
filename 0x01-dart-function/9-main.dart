fda537b2fb43cdfadbab9fead73cac49d2a2f4c5
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-19 21:02:34 -0400

    Make sure exercise 9 is working properly

--- 0x01-dart-function/9-main.dart
+++ 0x01-dart-function/9-main.dart
@@ -1,7 +1,18 @@
 import '9-palindrome.dart';
 
 main() {
-    print(isPalindrome("aa"));
     print(isPalindrome("abcba"));
     print(isPalindrome("abcde"));
+    print(isPalindrome("racecar"));
+    print(isPalindrome("helloworld"));
+    print(isPalindrome("ooo"));
+    print(isPalindrome("oo"));
+    print(isPalindrome("o"));
+    print(isPalindrome("oooooooooooo"));
+    print(isPalindrome("123454321"));
+    print(isPalindrome("1234543221"));
+    print(isPalindrome("1234554321"));
+    print(isPalindrome("lkeshfakewjhflkwajehfd09z87wa0938r71'R2LK"));
+    print(isPalindrome("S;LJF;LESRJGPOZ"));
+    print(isPalindrome("uneven"));
 }
