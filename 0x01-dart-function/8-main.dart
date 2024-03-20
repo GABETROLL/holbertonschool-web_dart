3678ef2b40df56f271d4f95475f08abc6bdb4854
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-19 20:57:21 -0400

    Make sure exercise 8 is working properly

--- 0x01-dart-function/8-main.dart
+++ 0x01-dart-function/8-main.dart
@@ -4,4 +4,11 @@ main() {
     print(longestUniqueSubstring("abcacbd"));
     print(longestUniqueSubstring("aaaaaaaa"));
     print(longestUniqueSubstring("abcde"));
+    print(longestUniqueSubstring("aaaaaaaaaaaaaaaabaaaaaababaabaaaaaaaaabababaaaaababab"));
+    print(longestUniqueSubstring("ghewjfhaaiuewhfowie7f0987es0f987waef098zdsf"));
+    print(longestUniqueSubstring("123456789012345678901234567890"));
+    print(longestUniqueSubstring("elakdhfjk8034"));
+    print(longestUniqueSubstring("oweyufpowaieugifdz[p]"));
+    print(longestUniqueSubstring("32875q9jvtwWA;4L' REE[]F ][PDSAF P3WARV 3Q087S]"));
+    print(longestUniqueSubstring("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAb"));
 }
