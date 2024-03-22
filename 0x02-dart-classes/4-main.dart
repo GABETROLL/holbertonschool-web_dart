e71b97aef8a50091ba25a80ae01abff08c70490e
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-22 14:14:18 -0400

    Add new 4's main

--- 0x02-dart-classes/4-main.dart
+++ 0x02-dart-classes/4-main.dart
@@ -1,7 +1,7 @@
 import '4-mutables.dart';
 
 void main() {
-    final password = Password(password: "Passwordecode");
+    final password = new Password(password: "Passwordecode");
     print(password.password);
     // print(password.password()); // Error: 'password' isn't a function or method and can't be invoked.
     // print(password._password); // Error: The getter '_password' isn't defined for the class 'Password'.
@@ -13,6 +13,12 @@ void main() {
     // print(password.password()); // Error: 'password' isn't a function or method and can't be invoked.
     // print(password._password); // Error: The getter '_password' isn't defined for the class 'Password'.
     // print(password._password()); // Error: The method '_password' isn't defined for the class 'Password'
+    print(password.isValid());
+    print(password);
+
+    // password.password = 5; // Error: A value of type 'int' can't be assigned to a variable of type 'String'.
+    password.password = "";
+    print(password.password);
     print(password.isValid());
     print(password);
 }
