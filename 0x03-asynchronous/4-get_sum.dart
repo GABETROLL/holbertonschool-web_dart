f97efea437b722d3a5b0f7c5a3c1ef2b4a1fb177
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-25 13:51:42 -0400

    Add Dart async exercise 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import '4-util.dart';
+
+Future<double> calculateTotal() async {
+    try {
+        final userId = json.decode(await fetchUserData())['id'];
+        final userOrders = json.decode(await fetchUserOrders(userId));
+
+        double total = 0.0;
+
+        // if (userOrders == null) return total;
+
+        for (final product in userOrders) {
+            final productPrice = json.decode(await fetchProductPrice(product));
+            total += productPrice;
+        }
+
+        return total;
+    } catch (error) {
+        return -1;
+    }
+}
