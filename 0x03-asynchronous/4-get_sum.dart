e1a11384d6bfb4d86b708c52af2bf1d9707b0f04
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-26 19:50:02 -0400

    Correct null price error in Dart - Async Task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,15 +3,16 @@ import '4-util.dart';
 
 Future<double> calculateTotal() async {
     try {
-        final userId = json.decode(await fetchUserData())['id'];
-        final userOrders = json.decode(await fetchUserOrders(userId));
+        final id = json.decode(await fetchUserData())['id'];
+        final userOrders = json.decode(await fetchUserOrders(id));
 
         double total = 0.0;
 
-        if (userOrders == null) return total;
+        if (userOrders == null) return total; // ?
 
         for (final product in userOrders) {
             final productPrice = json.decode(await fetchProductPrice(product));
+            if (productPrice == null) continue; // ?
             total += productPrice;
         }
 
