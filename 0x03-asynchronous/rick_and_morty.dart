fb34bbd404c14371f72f0b9e4015b70ac18d2b75
GABETROLL <gabriel7icplhappy@gmail.com>
2024-03-29 11:47:10 -0400

    Only print first page of characters

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,21 +3,17 @@ import 'dart:convert';
 
 Future<void> printRmCharacters() async {
     try {
-        String? nextPageUrl = 'https://rickandmortyapi.com/api/character';
+        String url = 'https://rickandmortyapi.com/api/character';
 
-        while (nextPageUrl != null) {
-            http.Response charactersResponse = await http.get(
-                Uri.parse(nextPageUrl),
-            );
-            if (charactersResponse.statusCode != 200) return;
+        http.Response charactersResponse = await http.get(
+            Uri.parse(url),
+        );
+        if (charactersResponse.statusCode != 200) return;
 
-            Map<String, dynamic> charactersBody = json.decode(charactersResponse.body);
+        Map<String, dynamic> charactersBody = json.decode(charactersResponse.body);
 
-            for (final Map<String, dynamic> character in charactersBody['results']) {
-                print(character['name']);
-            }
-
-            nextPageUrl = charactersBody['info']['next'];
+        for (final Map<String, dynamic> character in charactersBody['results']) {
+            print(character['name']);
         }
     } catch (error) {
         print('error caught: $error');
